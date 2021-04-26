0c556dd8dc22109ecf2466f5f6f0791de7e5aefb
estefano <sandulescustefano8@gmail.com>
2021-04-26 21:28:04 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+        var  numerosaleatorios  = 0
+
+
+        val listaNumerosAleatorios = mutableListOf < Int > ()
+
+        for(i in 0..numerosaleatorios){
+            listaNumerosAleatorios.add((0..100).random())
+            println(i)
+        }
+
+
+
+
+
+    }
+
