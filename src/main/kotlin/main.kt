32cb086119ae966f9dbeb25e28d2df455f6831ea
Estefano22 <sandulescustefano8@gmail.com>
2021-04-29 09:15:20 +0200

    hecho

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,18 @@
 fun main() {
 
     var numerosAleatorio = 0
+    var media = 0
     val listadelosNumeros = mutableListOf<Int>()
-    var media : Double = 0.0
+
     val numerosMayoresoIguales: MutableList<Int> = mutableListOf()
 
     repeat (100){
         numerosAleatorio = (0..100).random()
         listadelosNumeros.add(numerosAleatorio)
         media += numerosAleatorio
-        media /= listadelosNumeros.size
+        listadelosNumeros.add(numerosAleatorio)
     }
+    media /= listadelosNumeros.size
     for (i in listadelosNumeros){
         if (i >= media) numerosMayoresoIguales.add(i)
 
@@ -18,7 +20,7 @@ fun main() {
 
     println("La media es: $media \n" )
 
-    println("Numeros mayores o iguales que la media : \n $numerosMayoresoIguales \n")
+    println("Numeros mayores o iguales que la media : $numerosMayoresoIguales")
 
 
     }
